20a5d3fc333cbeb4160cacb235ff0ea0a095d77b
Benjamin Benjaminsen <benjamin@booleanbyte.com>
2017-09-18 14:40:22 +0200

    Adding sodatype fanta and added to vending machine

--- main.cpp
+++ main.cpp
@@ -21,10 +21,17 @@ int main()
 
     // Create a cola object. Constructor will be called with given parameters.
     Soda cola("Colabrus", 34, 4);
+
+    // Creating a soda type fanta
+    Soda fanta("Fanta", 20, 400);
+
     // We have a variable named cola with Soda as its data type.
 
     // Call the VendingMachine addType function with cola variable as parameter.
     vendingMachine.addType(cola);
+    // Adding the sodatype fantat ot inventory
+    vendingMachine.addType(fanta);
+
     // Print inventory using public printInventory member function (method).
     vendingMachine.printInventory();
 
